301f85f527c6e80523bb0aa14bdaee69ae5b8114
Prelipcean Cristian <prelipceancristian100@gmail.com>
2020-09-05 04:01:31 +0300

    added meal screens and basic singe meal screens functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
+import 'screens/meal_detail_screen.dart';
+import 'screens/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,6 +29,15 @@ class MyApp extends StatelessWidget {
             )),
       ),
       home: CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+      },
+      onUnknownRoute: () {},
     );
   }
 }
